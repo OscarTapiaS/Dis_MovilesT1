734458312b7d078471c397eb7fb8c84cbf47e13c
OscarTapiaS <143676242+OscarTapiaS@users.noreply.github.com>
2025-09-05 22:55:36 -0600

    Add files via upload

---
+++ src/main/java/com/example/tarea1/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.tarea1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tarea1.ui.theme.Tarea1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tarea1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black) // Fondo negro para contraste
+            .padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = """
+            *          *        *         *
+          ***          **********          ***
+       *****           **********           *****
+     *******           **********           *******
+   **********         ************         **********
+  ****************************************************
+ ******************************************************
+********************************************************
+********************************************************
+********************************************************
+ ******************************************************
+  ********      ************************      ********
+   *******       *     *********      *       *******
+     ******             *******              ******
+       *****             *****              *****
+          ***             ***              ***
+            **             *              **
+                                
+            """.trimIndent(),
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            fontSize = 11.sp,
+            color = Color.Yellow, // Asteriscos amarillos
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tarea1Theme {
+        Greeting()
+    }
+}
